1ed4afcc34506f1becadc5e651ceb976fdd8937d
Illia Berezin <berezin.main@gmail.com>
2025-02-18 22:07:23 +0200

    Lesson 3 home work 2

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log(1);
+let name = prompt("Whats is your name?");
+let age = prompt("How old are you?");
+let gender = prompt("What is your gender?");
+let country = prompt("Where are you from?");
+
+let info = `Your name is ${name}. You are ${age} years old ${gender} from ${country}.`;
+console.log(info);
