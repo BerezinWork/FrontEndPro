5dc157a5902437b3c14eb553ed5c573cb2a156e8
Illia Berezin <berezin.main@gmail.com>
2025-02-18 21:56:59 +0200

    Lesson 3 home work 1

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log(5);
+let num = 1;
+let str = "Illia";
+let bool = true;
+let obj = {};
+let undef;
+
+console.log(typeof (num));
+console.log(typeof (str));
+console.log(typeof (bool));
+console.log(typeof (obj));
+console.log(typeof (undef));
